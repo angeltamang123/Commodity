9882d25f12b9f8f43814b800c60edf6853ee8119
angel_tamang <tamangangel2057@gmail.com>
2024-07-15 18:01:55 +0545

    connection to mongodb

--- server/src/index.js
+++ server/src/index.js
@@ -5,6 +5,9 @@ const port = 7000;
 const userRoute = require("./routes/users");
 app.use(userRoute);
 
+const connect = require("./db/connection"); //importing db
+connect(); //function for connecting db
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
