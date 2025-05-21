8efccc924847c2566867522cf97f25c23c97ec01
angel_tamang <tamangangel2057@gmail.com>
2025-05-21 17:44:43 +0545

    style:more chatbox

--- client/src/components/chat-card.js
+++ client/src/components/chat-card.js
@@ -1,3 +1,4 @@
+"use client";
 import {
   Card,
   CardContent,
@@ -10,6 +11,8 @@ import React from "react";
 import Sidebar from "./sidebar";
 import { Input } from "./ui/input";
 import { SendHorizonal } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Textarea } from "./ui/textarea";
 
 const ChatCard = () => {
   const chat = [
@@ -18,41 +21,64 @@ const ChatCard = () => {
     { text: "How you doing!!", sender: "me" },
   ];
   return (
-    <Card className="relative min-h-screen flex justify-center items-center -mt-32">
-      <div className="flex gap-0 border-black border-2 rounded-xl overflow-hidden">
-        <Sidebar />
-        <div>
-          <CardHeader className="bg-slate-400 p-1 pl-6">
-            <CardTitle>Hari</CardTitle>
-            <CardDescription className="text-xs">
-              End-to-End encrypted
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="h-40 flex flex-col w-96">
-            <p>Chat logs of hari</p>
-            <div className="overflow-hidden">
-              {chat.map((item) =>
+    <div className="absolute min-h-screen w-full flex grow justify-center items-center ">
+      <Card className=" flex justify-center items-center mt-12 h-[500px]">
+        <div className="flex gap-0 border-black border-2 rounded-xl h-full overflow-hidden">
+          <Sidebar />
+          <div>
+            <CardHeader className="flex flex-row bg-[#960000] border-b-1 borfer-black p-1 pl-6">
+              <Avatar>
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <div className="mx-5">
+                <CardTitle>Hari</CardTitle>
+                <CardDescription className="text-xs text-gray-800">
+                  End-to-End encrypted
+                </CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent className="h-[70%] flex flex-col w-[600px] overflow-x-hidden overflow-y-scroll gap-2 my-2">
+              {chat.map((item, index) =>
                 item.sender == "me" ? (
-                  <div className="flex flex-col w-10/12 ml-16 bg-blue-700 ">
-                    <p>me</p>
+                  <div
+                    key={index}
+                    className="flex flex-col w-[75%] border-1 rounded-md ml-36 p-1 font-light bg-[#307ebe] "
+                  >
                     <p>{item.text}</p>
                   </div>
                 ) : (
-                  <div className="flex flex-col w-10/12 bg-gray-400">
-                    <p>Hari</p>
-                    <p>{item.text}</p>
+                  <div key={index} className="flex w-[75%] gap-2 ">
+                    <Avatar>
+                      <AvatarImage
+                        src="https://github.com/shadcn.png"
+                        alt="@shadcn"
+                      />
+                      <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <p className="border-1 rounded-md w-full p-1 font-light bg-gray-500">
+                      {item.text}
+                    </p>
                   </div>
                 )
               )}
-            </div>
-          </CardContent>
-          <CardFooter className="gap-2 border-t-2 border-black">
-            <Input placeholder="Enter your chat to Hari"></Input>
-            <SendHorizonal />
-          </CardFooter>
+            </CardContent>
+            <CardFooter className="gap-2 border-t-1 p-2 border-black">
+              <Textarea
+                placeholder="Enter to chat"
+                className="border-gray-500 resize-none overflow-hidden leading-5"
+                rows={1}
+              />
+
+              <SendHorizonal />
+            </CardFooter>
+          </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </div>
   );
 };
 
